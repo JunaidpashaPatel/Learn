3cc78ed5e497a401f8d000acdbdf1f54d30a36a8
Junaidpasha <junaidpashapatel92@gmail.com>
2025-01-26 21:12:47 +0530

    Commited by Jp

--- One2.java
+++ One2.java
@@ -4,5 +4,7 @@ public class One2 {
     public static void main(String[] args) {
         System.out.println("Hi this file One2 in the on folder!!!");
         System.out.println("One2 folder is changed");
+        System.out.println("Update the code afetr the push");
+        System.out.println("Lets learn the Git and Git Hub!!!!!!!!!");
     }
 }
