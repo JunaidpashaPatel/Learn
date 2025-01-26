fd4214df3bac3e5726f1620107442e6519a9b9cc
Junaidpasha <junaidpashapatel92@gmail.com>
2025-01-26 10:40:57 +0530

    One folder add

---
+++ One1.java
@@ -0,0 +1,6 @@
+package one;
+public class One1{
+    public static void main(String[] args) {
+        System.out.println("Hi this is in one folder's One1 file");
+    }
+}
---
+++ One2.java
@@ -0,0 +1,7 @@
+package one;
+
+public class One2 {
+    public static void main(String[] args) {
+        System.out.println("Hi this file One2 in the on folder!!!");
+    }
+}
