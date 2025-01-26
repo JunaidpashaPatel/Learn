a461e9cbdd05307e2da0b3a06972085c521a98fa
Junaidpasha <junaidpashapatel92@gmail.com>
2025-01-26 10:41:59 +0530

    One2 file is updated

--- One2.java
+++ One2.java
@@ -3,5 +3,6 @@ package one;
 public class One2 {
     public static void main(String[] args) {
         System.out.println("Hi this file One2 in the on folder!!!");
+        System.out.println("One2 folder is changed");
     }
 }
